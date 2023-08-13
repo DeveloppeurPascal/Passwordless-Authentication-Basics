d60cd6241a46cd2f0e96aad19fe4138ca7023834
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-08-13 16:59:36 +0200

    created the database SQL script

---
+++ database.sql
@@ -0,0 +1,23 @@
+START TRANSACTION;
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `enabled` bigint(1) NOT NULL DEFAULT 0,
+  `create_IP` varchar(255) NOT NULL DEFAULT '',
+  `create_datetime` char(14) NOT NULL DEFAULT '00000000000000',
+  `email_checked` bigint(1) NOT NULL DEFAULT 0,
+  `email_check_ip` varchar(255) NOT NULL DEFAULT '',
+  `email_check_datetime` char(14) NOT NULL DEFAULT '00000000000000'
+) DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`,`id`);
+
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+COMMIT;
+
+alter table users add comp blob not null default '';
